f0e0a4dcff8b46558a5765169971700df659240e
RSM <rsmRSM@Michels-iMac.local>
2023-02-28 21:39:52 -0500

    auto commit after session end

--- datacollector.h
+++ datacollector.h
@@ -12,6 +12,6 @@ int32_t GetSessionTime(const DataCollector &collector);
 bool AddData(DataCollector &collector, uint32_t time, uint16_t value);
 void WriteDataSession(DataCollector &collector);
 void InitDataSession(DataCollector &collector, uint32_t time);
-bool IsExpired(const DataCollector &collector);
+bool IsExpired(const DataCollector &collector, uint32_t time);
 void ClearStorage(DataCollector &collector);
 void Display(const DataCollector &collector, uint32_t minTime);
